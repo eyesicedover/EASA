c9950982144bc7369e141de4803f5ec85784fbda
Johnny Mayer and Stephanie Faber <johnny+stephanie@>
2018-04-24 13:51:01 -0700

    Add test for fuel calc, method for fuel calc

---
+++ EASA.Tests/ModelTests/FuelTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EASA.Models;
+
+namespace EASA.Tests
+{
+    [TestClass]
+    public class FuelTests
+    {
+        [TestMethod]
+        public void DoesCalcFuelWeight_ReturnFuelWeight_Double ()
+        {
+            //Arrange
+            var newFuel = new Fuel(1000,1.4);
+ 
+            //Act
+            var result = newFuel.CalcFuelWeight();
+
+            //Assert
+            Assert.AreEqual(595, result);
+          
+        }
+    }
+}
--- EASA/Models/Fuel.cs
+++ EASA/Models/Fuel.cs
@@ -7,5 +7,19 @@ namespace EASA.Models
 {
     public class Fuel
     {
+        public double DryWeight { get; set; }
+        public double FinalSpeed { get; set; }
+
+        public Fuel(double weight, double speed)
+        {
+            DryWeight = weight;
+            FinalSpeed = speed;
+        }
+
+        public double CalcFuelWeight()
+        {
+            var result = Math.Ceiling(DryWeight * Math.Pow(2.718, (FinalSpeed / 3)) - DryWeight);
+            return result;
+        }
     }
 }
